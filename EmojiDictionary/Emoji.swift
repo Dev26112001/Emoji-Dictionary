6e2ee4439f649c9b62ceb0e4426fa9c42f0a5c89
Devjyoti Mohanty <68328634+Dev26112001@users.noreply.github.com>
2023-01-13 15:17:30 +0530

    Add files via upload

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Devjyoti Mohanty on 11/01/23.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
